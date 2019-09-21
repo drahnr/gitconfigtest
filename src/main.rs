47421f89bcd31580fa243be5b78280be3167529d
Bernhard Schuster <bernhard@ahoi.io>
2019-09-21 13:27:04 +0200

    show that the config obj is actually fine

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ fn main() {
     let loco = std::env::current_dir().expect("CWD env var not set properly");
 	let repo = git2::Repository::open(&loco).expect("cwd should be a repo");
     let cfg = repo.config().expect("Failed to obtain config struct");
+    for entry in &cfg.entries(None).unwrap() {
+        let entry = entry.unwrap();
+        println!("{} => {}", entry.name().unwrap(), entry.value().unwrap());
+    }
     let name = cfg.get_str("user.name").unwrap();
     let email = cfg.get_str("user.email").unwrap();
     println!("Found {} <{}> ", name, email);
